fed2895a560776817987f3755a3fe86c29602e9f
AvdeyYy <sergeyavdeyy@gmail.com>
2023-08-17 04:23:46 +0700

    first commit

---
+++ src/test/java/com/avdeyy/SocialMediaApi/SocialMediaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avdeyy.SocialMediaApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
